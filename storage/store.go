0a27d31559e483a2096d1519f3b997f63c7d3992
sawoklybimecpubliki <sawoklybimec@gmail.com>
2024-11-18 19:39:35 +0300

    add func

--- storage/store.go
+++ storage/store.go
@@ -1,42 +1,81 @@
 package storage
 
 import (
-	"context"
 	"errors"
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 )
 
 type Database interface {
-	AddNewUser(ctx context.Context, u User) error
-	DeleteUser(ctx context.Context, id string) error
-	All(ctx context.Context) ([]User, error)
-	Authentication(ctx context.Context, u User) (string, error)
+	AddNewUser(u User) error
+	Del(id string) error
+	All() []User
+	Authentication(u User) error
 }
 
 type User struct {
-	Login     string `json:"Login" bson:"login"`
-	Password  string `json:"Password" bson:"password"`
-	IdStorage string `json:"omitempty" bson:"storageId"`
+	Login    string `json:"Login"`
+	Password string `json:"Password"`
 }
 
-// TODO auth  отнести к юзеру а в базе возвращать данные пользователя
-
-func (u *User) hash() (string, error) {
+func (u *User) hash() ([]byte, error) {
 	passHash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
 	if err != nil {
 		slog.Error("failed to generate password hash", err)
-		return "", err
+		return []byte{}, err
 	}
-	return string(passHash), nil
+	return passHash, nil
 }
 
 type DataBase struct {
-	Data map[string]string
+	Data map[string][]byte
 }
 
-// TODO возможно переделать или убрать вообшще, в Authentication она не используется
+// возможно переделать или убрать вообшще, в Authentication она не используется
 func (d *DataBase) compareLogin(loginNewUser string) error {
 	for login := range d.Data {
 		if login == loginNewUser {
-			return errors.New("Uncorrect username") // отдать пользователю о
+			return errors.New("Uncorrect username") // отдать пользователю ошибку, но не сообщать что есть такой пользователь
+		}
+	}
+	return nil
+}
+
+func (d *DataBase) AddNewUser(u User) error {
+	if err := d.compareLogin(u.Login); err != nil {
+		return err
+	}
+	if p, err := u.hash(); err == nil {
+		d.Data[u.Login] = p
+	} else {
+		return err
+	}
+	return nil
+}
+
+func (d *DataBase) Authentication(u User) error {
+	if _, ok := d.Data[u.Login]; !ok {
+		return errors.New("user not found")
+	}
+	if err := bcrypt.CompareHashAndPassword(d.Data[u.Login], []byte(u.Password)); err != nil {
+		return errors.New("invalid password")
+	}
+
+	return nil
+}
+
+func (d *DataBase) Del(id string) error {
+	if _, ok := d.Data[id]; !ok {
+		return errors.New("user not found")
+	}
+	delete(d.Data, id)
+	return nil
+}
+
+func (d *DataBase) All() []User {
+	var u []User
+	for login, password := range d.Data {
+		u = append(u, User{login, string(password)})
+	}
+	return u
+}
